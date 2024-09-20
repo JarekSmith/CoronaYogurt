e2ea0268e6dff257e747879d39d88d4f93230ebf
JarekSmith <105569335+JarekSmith@users.noreply.github.com>
2024-09-20 10:09:33 -0600

    Google Calendar Implementation
    Integrated Google Calendar API to get upcoming events from the Corona Yogurt calendar
    
    Also updated to the latest version of WintonCraft

--- script.js
+++ script.js
@@ -1,9 +1,73 @@
-document.addEventListener("DOMContentLoaded", function() {
-    eventMonth();
-})
+// Client ID and API Key from Google Cloud
+const CLIENT_ID = '236413033644-1h7sh3i3b8cuodr7rctn3sm3ldm63q8e.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyBi6xu6JW9eIbETa3e4VLIYxrKu30Map4Q';
 
-function eventMonth() {
-    let monthP = document.getElementById("events").children[1];
+// Discovery doc URL
+const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
+
+// Authorization scopes required by the API
+const SCOPES = 'https://www.googleapies.com/auth/calendar.readonly https://www.googleapies.com/auth/calendar.events.readonly';
+
+let tokenClient;
+let gapiInited = false;
+let gisInited = false;
+
+const CALENDAR_ID = '330cc876873512068a03eb9b14175964f3667de7ee0f660e773641212a93ccbd@group.calendar.google.com'; // Calendar ID of the Corona Yogurt calendar
+
+// Callback after api.js is loaded
+function gapiLoaded() {
+    gapi.load('client', initializeGapiClient);
+}
+
+async function initializeGapiClient() {
+    await gapi.client.init({
+        apiKey: API_KEY,
+        discoveryDocs: [DISCOVERY_DOC],
+    });
+    gapiInited = true;
+    await listUpcomingEvents();
+}
+
+function gisLoaded() {
+    tokenClient = google.accounts.oauth2.initTokenClient({
+        client_id: CLIENT_ID,
+        scope: SCOPES,
+        callback: '', // defined later
+    });
+    gisInited = true;
+}
+
+async function listUpcomingEvents() {
+    let response;
+    try {
+        const request = {
+            'calendarId': CALENDAR_ID,
+            'timeMin' : (new Date()).toISOString(),
+            'showDeleted': false,
+            'singleEvents': true,
+            'maxResults': 10,
+            'orderBy': 'startTime',
+        };
+        response = await gapi.client.calendar.events.list(request);
+    } catch (err) {
+        document.getElementById('eventsList').innerText = err.message;
+        return;
+    }
+
+    const events = response.result.items;
+    if (!events || events.length == 0) {
+        NoEvents();
+        return;
+    }
+    // Flatten to string to display
+    const output = events.reduce(
+        (str, event) => `${str}</li><li><strong>${event.summary}</strong> (${event.start.dateTime || event.start.date})</li>`, '<li>');
+        document.getElementById('eventsList').innerHTML = `<ul>${output}</ul>`;
+        document.getElementById('eventsList').innerHTML = document.getElementById('eventsList').innerHTML.replace("<li></li>", "");
+}
+
+function NoEvents() {
+    let monthP = document.getElementById("eventsList");
     const d = new Date()
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let currentMonth = months[d.getMonth()]
