ec06462eae7240e73b16106ef183de8d746518ce
Jarek Smith <jarek.smith.88@gmail.com>
2026-06-23 22:51:50 -0600

    Add server status indicators

--- script.js
+++ script.js
@@ -18,3 +18,32 @@ function toggleKojima() {
             gokuSpin.src = gokuImage;
     }
 }
+
+async function addStatusIndicators() {
+    const indicators = document.getElementsByClassName("status");
+    const svgNS = "http://www.w3.org/2000/svg";
+    const activeIcon = document.createElementNS(svgNS, "svg");
+    activeIcon.setAttribute("width", "16");
+    activeIcon.setAttribute("height", "16");
+    const circle = document.createElementNS(svgNS, "circle");
+    circle.setAttribute("cx", "8");
+    circle.setAttribute("cy", "8");
+    circle.setAttribute("r", "6");
+    circle.setAttribute("fill", "red");
+
+    activeIcon.appendChild(circle);
+    const inactiveIcon = activeIcon.cloneNode(true);
+    inactiveIcon.childNodes[0].setAttribute("fill", "green");
+    [...indicators].forEach(indicator => indicator.appendChild(activeIcon.cloneNode(true)));
+
+    let response = await fetch("https://api.mcstatus.io/v2/status/java/mc.fartsound.us");
+    if (!response.ok) throw new Error(`Response status: ${response.status}`);
+    let result = await response.json();
+    if (result.online) indicators[0].getElementsByTagNameNS(svgNS, "circle")[0].setAttribute("fill", "green");
+    response = await fetch("https://api.mcstatus.io/v2/status/java/play.fartsound.us");
+    if (!response.ok) throw new Error(`Response status: ${response.status}`);
+    result = await response.json();
+    if (result.online) indicators[1].getElementsByTagNameNS(svgNS, "circle")[0].setAttribute("fill", "green");
+}
+
+document.addEventListener("DOMContentLoaded", async () => {await addStatusIndicators()});
